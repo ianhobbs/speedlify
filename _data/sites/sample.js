26fb1b66b5f1ee394da4d649b7ea4cdc66372fb1
ian hobbs <admin@ianhobbsmedia.com>
2021-08-10 14:29:20 +1000

    tweaking codey

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "Codey", // optional, falls back to object key
-	description: "Codey Home site",
+	description: "codey.net.au",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
